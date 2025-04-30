2dbd39a69ef9ea47dc468303b929953c56ad7651
yangjiabin <706948683@qq.com>
2025-04-30 16:58:44 +0800

    新增2道题

--- Code-suixianglu/src/二叉树/L108_将有序数组转换为二叉搜索树.java
+++ Code-suixianglu/src/二叉树/L108_将有序数组转换为二叉搜索树.java
@@ -22,10 +22,16 @@ public class L108_将有序数组转换为二叉搜索树 {
 
     class Solution {
         public TreeNode sortedArrayToBST(int[] nums) {
-
+            return recur(nums, 0, nums.length - 1);
         }
-        public TreeNode recur(int[] nums){
+        public TreeNode recur(int[] nums, int left, int right){
+            if (left > right || left >= nums.length || right < 0) return null;
 
+            int mid = (left + right) >> 1;
+            TreeNode treeNode = new TreeNode(nums[mid]);
+            treeNode.left = recur(nums, left, mid - 1);
+            treeNode.right = recur(nums, mid + 1, right);
+            return treeNode;
         }
     }
 }
